7b6ceebe8aad5cc4eeadb26f801b97561c5cb619
wargie <wargie@bk.ru>
2022-04-04 07:56:59 +0200

    Заданы массивы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arrayFirst =  {"labas", "hi", "pryvitannie", "privet"};
+int countFirst = CountStringLessThreeChar(arrayFirst);
+string[] resultFirst =  FillResultArray(arrayFirst,countFirst);
+
+
+string[] arraySecond = {"1234", "1567", "-2", "computer science"};
+int countSecond  = CountStringLessThreeChar(arraySecond);
+string[] resultSecond = FillResultArray(arraySecond,countSecond);
+
+
+string[] arrayThird = {"Poland", "Lithuania", "Belarus"};
+int countThird = CountStringLessThreeChar(arrayThird);
+string[] resultThird = FillResultArray(arrayThird, countThird);
+
+
+PrintArray(resultFirst);
+PrintArray(resultSecond);
+PrintArray(resultThird);
