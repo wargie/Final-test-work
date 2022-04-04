1ae20bed1597d6306309dbadd6fb0fd7bb667277
wargie <wargie@bk.ru>
2022-04-04 08:03:58 +0200

    Определены методы

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ int countFirst = CountStringLessThreeChar(arrayFirst);
 string[] resultFirst =  FillResultArray(arrayFirst,countFirst);
 
 
-string[] arraySecond = {"1234", "1567", "-2", "computer science"};
+string[] arraySecond = {"1234", "16", "-41", "my_sun"};
 int countSecond  = CountStringLessThreeChar(arraySecond);
 string[] resultSecond = FillResultArray(arraySecond,countSecond);
 
@@ -11,6 +11,19 @@ string[] resultSecond = FillResultArray(arraySecond,countSecond);
 string[] arrayThird = {"Poland", "Lithuania", "Belarus"};
 int countThird = CountStringLessThreeChar(arrayThird);
 string[] resultThird = FillResultArray(arrayThird, countThird);
+
+void PrintArray(string[] array) 
+{
+
+    for (int i = 0; i < array.Length; i++)
+        {
+            if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+            else Console.Write($"{array[i]}.");
+        }
+    Console.WriteLine();
+}
+
+
 
 
 PrintArray(resultFirst);
