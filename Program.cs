6351f9a3d1b82660497cc59701c770874621e10b
wargie <wargie@bk.ru>
2022-04-04 08:07:44 +0200

    Отладка программы

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] arrayFirst =  {"labas", "hi", "pryvitannie", "privet"};
+﻿string[] arrayFirst =  {"labas", "hi", "pryvitannie", "privet", "Ey"};
 int countFirst = CountStringLessThreeChar(arrayFirst);
 string[] resultFirst =  FillResultArray(arrayFirst,countFirst);
 
@@ -8,23 +8,44 @@ int countSecond  = CountStringLessThreeChar(arraySecond);
 string[] resultSecond = FillResultArray(arraySecond,countSecond);
 
 
-string[] arrayThird = {"Poland", "Lithuania", "Belarus"};
+string[] arrayThird = {"Poland", "Lithuania", "Belarus", "OAE"};
 int countThird = CountStringLessThreeChar(arrayThird);
 string[] resultThird = FillResultArray(arrayThird, countThird);
 
 void PrintArray(string[] array) 
-{
+    {
 
-    for (int i = 0; i < array.Length; i++)
-        {
-            if (i != array.Length - 1) Console.Write($"{array[i]}, ");
-            else Console.Write($"{array[i]}.");
-        }
-    Console.WriteLine();
-}
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+                else Console.Write($"{array[i]}.");
+            }
+        Console.WriteLine();
+    }
 
+int CountStringLessThreeChar(string[] array)
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3) count++;
+            }
+        return count;
+    }
 
-
+string[] FillResultArray(string[] array, int count)
+    {
+        string[] result = new string[count];
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3)
+                    {
+                        result[result.Length - count] = array[i];
+                        count--;
+                    }
+            }
+        return result;
+    }
 
 PrintArray(resultFirst);
 PrintArray(resultSecond);
